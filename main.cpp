0ff788672b19d4b8e029026fec82c769fa672a84
huyuxuab <hu20020207@gmail.com>
2017-06-26 12:27:09 +0800

    bjt 17.6.26 12.27

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+void reset(){
+    system("cls");
+    system("title Knowledge_base");
+}
 int main() {
-    return 0;
+    do{
+        reset();
+        cout<<endl<<" Originally by Calvin Hsu"<<endl;
+        cout<<endl<<" ======================"<<endl;
+        cout<<endl<<" c++ version by rh_hu"<<endl;
+        cout<<endl<<" Constructions:"<<endl;
+
+        return 0;
+    }while(1);
 }
