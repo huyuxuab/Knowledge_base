859549e7c0856742d9fe9258a41819811616d961
huyuxuab <hu20020207@gmail.com>
2017-06-26 12:43:13 +0800

    bjt 17.6.26 12.27

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
+char inst;
 void reset(){
     system("cls");
     system("title Knowledge_base");
+}
+void quotations(){
+    reset();
+    
 }
 int main() {
     do{
@@ -11,8 +16,25 @@ int main() {
         cout<<endl<<" Originally by Calvin Hsu"<<endl;
         cout<<endl<<" ======================"<<endl;
         cout<<endl<<" c++ version by rh_hu"<<endl;
-        cout<<endl<<" Constructions:"<<endl;
-
-        return 0;
+        cout<<" instructions:"<<endl;
+        cout<<" q  - Quotations: learn from the elderly"<<endl;
+        cout<<" qt - Quick translate"<<endl;
+        cout<<" e  - Excited! Devote your life to the elderly"<<endl;
+        cout<<" r  - Request for more info"<<endl;
+        cout<<" Input instruction: ";
+        cin>>inst;
+        if(inst=='q'){
+            quotations();
+        }
+        if(inst=='qt'){
+            translate();
+        }
+        if(inst=='e'){
+            excited();
+        }
+        if(inst=='r'){
+            resource();
+        }
     }while(1);
+    return 0;
 }
